df4a4b4af6557f0ec467597fa3ff616c9b1cce1d
felipe <profissionalf.andre@gmail.com>
2024-10-05 11:51:49 -0300

    feat: add functions to slice and apply rules from CartOverview component to display total quantity and price in the cart

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,23 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit"
+import { Cart } from "./Cart"
 
-const INITIAL_STATE = {
-  // cart: [],
-  cart: [
-    {
-      pizzaId: 1,
-      name: "Vegetale",
-      quantity: 1,
-      unitPrice: 13,
-      totalPrice: 13,
-    },
-  ]
+interface CartState {
+  cart: Cart[]
+}
+
+const INITIAL_STATE: CartState = {
+  cart: [],
+}
+
+export type Actions = {
+  type: string
+  payload: Cart
 }
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState: INITIAL_STATE,
   reducers: {
-    addItem(state, action) {
+    addItem(state, action: Actions) {
       state.cart.push(action.payload)
     },
     deleteItem(state, action) {
@@ -51,3 +52,8 @@ const cartSlice = createSlice({
 
 export const { addItem, deleteItem, increaseItemQuantity, decreaseItemQuantity, clearCart } = cartSlice.actions
 export default cartSlice.reducer
+
+export const getCart = (state: { cart: CartState }) => state.cart.cart
+
+export const getTotalCartQuantity = (state: { cart: CartState }) =>state.cart.cart.reduce((acc, item) => acc + item.quantity, 0)
+export const getTotalCartPrice = (state: { cart: CartState }) =>state.cart.cart.reduce((acc, item) => acc + item.totalPrice, 0)
